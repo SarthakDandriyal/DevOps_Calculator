23c91051109a257104f7c2fda80276a6cf084fda
Sarthak Dandriyal <sarthakdandriyal3199@gmail.com>
2022-04-08 10:27:27 +0530

    Initial Commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,6 @@
+import java.io.*;
+public class Calculator {
+    public static void main(String args[])throws IOException{
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+    }
+}
